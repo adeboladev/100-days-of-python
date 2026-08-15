f80a9a6317f24878a60cdecf81bd1e7745ad939f
adeboladev <hardeyborlah38@gmail.com>
2026-08-15 12:42:01 +0100

    Add files via upload

---
+++ class8.py
@@ -0,0 +1,35 @@
+
+
+
+person = {
+      "name": "Adigun",
+      "age": 22,
+      "course": "info tech",
+      "hobby": "talking to his wife, me",
+}
+
+print(person.keys())
+print(person.values())
+print(person.items())
+
+for key,value in person.items():
+      print(key,":", value)
+      
+      
+for key in person.keys():
+      print(key)
+      
+for value in person.values():
+      print(value)
+      
+      
+for key, value in person.items():
+      print("Adigun's", key, "is", value)
+          
+      
+      
+      
+      
+      
+		
+      
